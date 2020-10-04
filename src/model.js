0da06a5c94cea2aee1bc591d8dbac92bc30d1931
DenisAleshkov <aleshkov592@gmail.com>
2020-10-04 20:55:30 +0300

    initial commit

---
+++ src/model.js
@@ -0,0 +1,22 @@
+export const model = [
+    {
+        type: 'title',
+        value: 'Hello World'
+    },
+    {
+        type: 'text',
+        value: 'some text'
+    },
+    {
+        type: 'columns',
+        value: [
+            '1111111',
+            '2222222',
+            '3333333'
+        ]
+    },
+    {
+        type: 'image',
+        value: './assets/1.png'
+    }
+]
